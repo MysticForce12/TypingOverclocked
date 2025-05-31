57bf3dec7657e730493f8c5342019462907591da
MysticForce12 <113507440+MysticForce12@users.noreply.github.com>
2025-06-01 01:42:24 +0530

    Fix socket bug and add score sync

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,6 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const socket = io('https://typeraijin.onrender.com/');
+const socket = io('https://typingoverclocked-4.onrender.com');
 
 export default socket;
